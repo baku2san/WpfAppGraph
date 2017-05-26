0be34c4347c7ca77d9f98165a81755f5d9aadc45
shimaTaka <takanori23@hotmail.co.jp>
2017-05-26 23:05:33 +0900

    Zone分割定数のIF用意

---
+++ WpfAppGraph/BindingSources/ListSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppGraph.BindingSources
+{
+    public class PercentageValueProvider: ListProvider<int>
+    {
+        // コンストラクタ
+        public PercentageValueProvider():base(Enumerable.Range(0, 100))
+        {
+            Console.WriteLine(Items);
+        }
+    }
+    public class ZoneAngleDivisionProvider : ListProviderInt
+    {
+        public ZoneAngleDivisionProvider() : base(3, 4)
+        {
+            Console.WriteLine(Items);
+        }
+    }
+    public class ZoneRadiusProvider : ListProviderInt
+    {
+        public ZoneRadiusProvider() : base(20,25)
+        {
+            Console.WriteLine(Items);
+        }
+    }
+    /// <summary>
+    /// params は、Generic未対応なので・・
+    /// </summary>
+    public class ListProviderInt
+    {
+        public List<int> Items { get; protected set; }
+        public ListProviderInt(params int[] items)
+        {
+            Items = items.ToList();
+        }
+    }
+    public class ListProvider<T>
+    {
+        public List<T> Items { get; protected set; }
+        public ListProvider(IEnumerable<T> items = null)
+        {
+            Items = new List<T>(items);
+        }
+    }
+}
